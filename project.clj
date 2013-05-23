f130d1ec0225dd74b480d72e986edc45cc58d795
Kyle Oba <koba@mudphone.com>
2013-05-22 16:19:18 -1000

    Add basic description and URL to project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject basic-metronome "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A basic Quil Clojure project template."
+  :url "http://github.com/mudphone/basic_quil_metronome"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
