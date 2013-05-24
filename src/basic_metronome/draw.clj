612b89129bf7418de985a209dc276f5604c28fca
Kyle Oba <koba@pasdechocolat.com>
2013-05-24 10:47:31 -1000

    Remove unnecessary function and extra multiply call.
    That was just unnecessary.  Why would you do that?

--- src/basic_metronome/draw.clj
+++ src/basic_metronome/draw.clj
@@ -2,17 +2,13 @@
   (:use [basic-metronome.setup :only [HEIGHT WIDTH tick]])
   (:require [quil.core :as qc]))
 
-(defn tick-tock
-  []
-  (swap! tick inc))
-
 (defn draw
   []
   (swap! tick inc)
   (qc/background 0 0 0)
   (qc/translate (* 0.5 WIDTH) (* 0.5 HEIGHT))
   (let [theta (* 0.05 @tick)
-        x (* (* 0.5 WIDTH) (Math/sin theta))]
+        x (* 0.5 WIDTH (Math/sin theta)]
     (qc/ellipse x 0 20 20)))
 
 
